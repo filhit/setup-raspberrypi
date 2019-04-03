61a4241c6cfacdebcd3ef653270528056435a821
Andrey Rjeutski <filhit@gmail.com>
2019-04-03 22:27:59 +0200

    Add nokia public key

--- 1-setup-keys.sh
+++ 1-setup-keys.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-PUB_KEY='ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILYmp1MFc552TYMnxX3eewRx4n8Rb3G/p09pl6Hq2Tkq filhit@FILHIT-VAIO'
+PUB_KEYS=(
+  'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILYmp1MFc552TYMnxX3eewRx4n8Rb3G/p09pl6Hq2Tkq filhit@FILHIT-VAIO'
+  'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILCddyFHaaLihs6MBWq2dLhvgTSE/x5NsbPHObxZoJ05 nokia key'
+)
+
 
 if [ ! -f "/home/pi/.ssh/id_ed25519" ]; then
   ssh-keygen -t ed25519 -o -a 100
@@ -7,7 +11,11 @@ fi
 
 umask 0077
 mkdir -p ~/.ssh
-grep -q -F "$PUB_KEY" ~/.ssh/authorized_keys 2>/dev/null || echo "$PUB_KEY" >> ~/.ssh/authorized_keys
+for i in ${!PUB_KEYS[*]}
+do
+  PUB_KEY=${PUB_KEYS[$i]}
+  grep -q -F "$PUB_KEY" ~/.ssh/authorized_keys 2>/dev/null || echo "$PUB_KEY" >> ~/.ssh/authorized_keys
+done
 
 echo "Add this to VAIO, github, nokia"
 printf '%b\n' "$(cat ~/.ssh/id_ed25519.pub)"
