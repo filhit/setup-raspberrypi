eb0f9776834db8212e930f646c2ca2385732e62e
Andrey Rjeutski <filhit@gmail.com>
2019-04-03 23:33:22 +0200

    Add ssh via tor hidden service instructions

---
+++ 3-setup-ssh-over-tor.sh
@@ -0,0 +1,12 @@
+sudo apt install tor
+sudo systemctl enable tor
+
+# Prepare sshd_config following https://stribika.github.io/2015/01/04/secure-secure-shell.html
+
+# Put the following lines to /etc/var/tor/torrc"
+# HiddenServiceDir /var/lib/tor/ssh_hidden_service/
+# HiddenServicePort 22 127.0.0.1:22
+
+sudo systemctl restart tor
+
+sudo cat /var/lib/tor/ssh_hidden_service/hostname
