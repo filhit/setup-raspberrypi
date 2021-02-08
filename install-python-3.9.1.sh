fe49c9378866d582ebd5c3add600a356ef7c9d15
filhit <filhit@gmail.com>
2021-02-08 23:06:23 +0100

    Fix Python 3.9.1 compilation dependencies

--- install-python-3.9.1.sh
+++ install-python-3.9.1.sh
@@ -2,7 +2,7 @@
 set -e
 
 #Install the packages
-sudo apt-get --assume-yes install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+sudo apt-get --assume-yes install build-essential python-dev python-setuptools python-pip python-smbus libncursesw5-dev libgdbm-dev libc6-dev zlib1g-dev libsqlite3-dev tk-dev libssl-dev openssl libffi-dev
 
 #Install python
 cd ~
